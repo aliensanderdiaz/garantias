bb3c887de2212d161d8800c07ef43fdf60ba586b
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-07-01 22:37:42 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,23 +1,6 @@
 let garantiasSinRevisar = [
-    '2025-04-30 - 08:24:47 - MCI081 - Oster - freidora 3.8oster - FALLA: falla el ventilador',
-    '2025-04-30 - 10:07:10 - EHX993 - Universal - freidora universal digital - FALLA: prende pero no calienta lo fuciente',
-    '2025-05-05 - 15:55:52 - UHX059 - Universal - ventilador fresh ultra - FALLA: prende pero no arranca',
-    '2025-05-06 - 11:57:06 - UNA790 - Oster - licuadora oster revetsible - FALLA: se apago y no volvio a prender',
-    '2025-05-09 - 11:02:34 - ONQ106 - Oster - horno freidora de aire 25 litros - FALLA: quema los alimentos y es la segunda vez que se recibe por la misma falla',
-    '2025-05-10 - 10:27:10 - UFO915 - Universal - licuadora universal - FALLA: no prenden',
-    '2025-05-10 - 11:08:38 - GEG319 - Universal - ventilador universal digita - FALLA: no prende y se recibe por segunda vez',
-    '2025-05-12 - 09:59:22 - KGW493 - Universal - termo universal de un litrl - FALLA: no calienta',
-    '2025-05-15 - 14:29:08 - JMY033 - Oster - freidora digital de 4 litos - FALLA: estaba en uso y de repoende la pantalla quedo negra y no prende',
-    '2025-05-15 - 14:42:54 - OHJ743 - Oster - freidor de 6 litos digital - FALLA: no lleva a termino la coccion',
-    '2025-05-17 - 11:03:21 - EYG069 - Universal - freidora universal digital - FALLA: prende pero no hace la funcion',
-    '2025-05-17 - 11:29:02 - OQA307 - Oster - licuadora oster clasica - FALLA: no prende el motor esta recalentado',
-    '2025-05-17 - 11:36:36 - GVP771 - Oster - licuadora oster clasica - FALLA: motor recalentado',
-    '2025-05-17 - 13:30:39 - IVO676 - Universal - danduchera de 4 puesto - FALLA: se recalento y quema y no prendio mas',
-    '2025-05-19 - 08:54:32 - WPG477 - Universal - plancha universal - FALLA: no prende',
-    '2025-05-20 - 09:11:08 - VHJ108 - Universal - aspiradora basic - FALLA: pieza derretida por recalentamiento',
-    '2025-05-21 - 09:55:02 - QQG130 - Oster - licuadora oster clasica - FALLA: se recalienta y esta desforzada',
-    '2025-05-21 - 10:06:22 - QKT387 - Universal - licuadora universal - FALLA: motor quemado',
-    '2025-05-21 - 14:41:56 - HRG299 - Universal - ventilado universal digital - FALLA: no ventila nada',
+'2025-06-25 - 10:37:17 - IRA561 - B&D - freidora black deckerr - FALLA: no prende ni calienta la señora ya la llevo a otro punto de servicio',
+'2025-06-28 - 15:48:16 - TBO541 - Garantía Universal - plancha corona - FALLA: se recalienta',
 ]
 
 // garantiasSinRevisar.reverse()
--- newells.js
+++ newells.js
@@ -1,1 +1,327 @@
-let garantiasNewells = []
+let garantiasNewells = [
+  {
+    "codigo": "WO-00374886",
+    "cliente": "Diana Marcela Cardona Lopez",
+    "estado": "En progreso",
+    "fecha": "30/06/2025",
+    "interno": "DEF281",
+    "fechaIngreso": "2025-06-26",
+    "productoIngreso": "freidora oster de 4 litros",
+    "lugarDeCompra": "EXITO",
+    "observaciones": ""
+  },
+  {
+    "codigo": "WO-00374884",
+    "cliente": "Luz Elvira Ortiz Tovar",
+    "estado": "En progreso",
+    "fecha": "30/06/2025",
+    "interno": "APO129",
+    "fechaIngreso": "2025-06-26",
+    "productoIngreso": "licuadora Oster reversible",
+    "lugarDeCompra": "EXITO",
+    "observaciones": "[martes, 1 de julio de 2025]alveiro pide tarjeta, yo pido motor y tarjeta"
+  },
+  {
+    "codigo": "WO-00374861",
+    "cliente": "Asesorias Y Operaciones Hoteleras Del Sur S.a.s.",
+    "estado": "En progreso",
+    "fecha": "30/06/2025",
+    "interno": "MSM840",
+    "fechaIngreso": "2025-06-15",
+    "productoIngreso": "licuadora oster clasica",
+    "lugarDeCompra": "ALMACENES UNIVERSAL",
+    "observaciones": ""
+  },
+  {
+    "codigo": "WO-00374858",
+    "cliente": "Luis Carlos Díaz Losada",
+    "estado": "En progreso",
+    "fecha": "30/06/2025",
+    "interno": "WGJ541",
+    "fechaIngreso": "2025-06-14",
+    "productoIngreso": "licuadora oster reversible",
+    "lugarDeCompra": "PAGINA DE OSTER",
+    "observaciones": "[martes, 1 de julio de 2025]alveiro pidio tarjeta, yo pido motor y tarjeta"
+  },
+  {
+    "codigo": "WO-00374853",
+    "cliente": "Kelly Yohana Claros",
+    "estado": "En progreso",
+    "fecha": "30/06/2025",
+    "interno": "FTE499",
+    "fechaIngreso": "2025-06-04",
+    "productoIngreso": "freidora oster",
+    "lugarDeCompra": "MEICO",
+    "observaciones": ""
+  },
+  {
+    "codigo": "WO-00368532",
+    "cliente": "Lizeth Carolina Benavidez Díaz",
+    "estado": "Entregado",
+    "fecha": "7/06/2025"
+  },
+  {
+    "codigo": "WO-00368531",
+    "cliente": "Luz Adriana Méndez",
+    "estado": "Entregado",
+    "fecha": "7/06/2025"
+  },
+  {
+    "codigo": "WO-00365949",
+    "cliente": "Gildardo Cajibion",
+    "estado": "Entregado",
+    "fecha": "29/05/2025"
+  },
+  {
+    "codigo": "WO-00364485",
+    "cliente": "Nancy Viviana Suarez Heredia",
+    "estado": "Entregado",
+    "fecha": "23/05/2025"
+  },
+  {
+    "codigo": "WO-00364481",
+    "cliente": "Lina Milena Samboni",
+    "estado": "Entregado",
+    "fecha": "23/05/2025"
+  },
+  {
+    "codigo": "WO-00364345",
+    "cliente": "Jairo Velasquez Rodriguez",
+    "estado": "Entregado",
+    "fecha": "23/05/2025"
+  },
+  {
+    "codigo": "WO-00364212",
+    "cliente": "Luz Elvira Ortiz",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00364210",
+    "cliente": "Carlos Andres Molina",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00364209",
+    "cliente": "Erika Mileidy Calderon",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00363906",
+    "cliente": "Jhon Alexander Calderon",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00363904",
+    "cliente": "Yorman Ortega",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00363903",
+    "cliente": "Empresa De Suministros, Logística Y Asesorías Integrales Azteca Sas",
+    "estado": "Entregado",
+    "fecha": "22/05/2025"
+  },
+  {
+    "codigo": "WO-00362370",
+    "cliente": "Tatiana Ramírez",
+    "estado": "Entregado",
+    "fecha": "16/05/2025"
+  },
+  {
+    "codigo": "WO-00357076",
+    "cliente": "Marleny Sterling Ramirez",
+    "estado": "Entregado",
+    "fecha": "28/04/2025"
+  },
+  {
+    "codigo": "WO-00357072",
+    "cliente": "Elcy Patricia Montenegro",
+    "estado": "Entregado",
+    "fecha": "28/04/2025"
+  },
+  {
+    "codigo": "WO-00357066",
+    "cliente": "Jaime Andres Meneses Cabrera",
+    "estado": "Entregado",
+    "fecha": "28/04/2025"
+  },
+  {
+    "codigo": "WO-00352731",
+    "cliente": "Juan Daniel Vargas",
+    "estado": "Entregado",
+    "fecha": "10/04/2025"
+  },
+  {
+    "codigo": "WO-00352730",
+    "cliente": "Almacén El Festival Del Hogar Florencia",
+    "estado": "Entregado",
+    "fecha": "10/04/2025"
+  },
+  {
+    "codigo": "WO-00351290",
+    "cliente": "Jova Perdomo",
+    "estado": "Entregado",
+    "fecha": "4/04/2025"
+  },
+  {
+    "codigo": "WO-00351274",
+    "cliente": "Lina Milena Samboni",
+    "estado": "Entregado",
+    "fecha": "4/04/2025"
+  },
+  {
+    "codigo": "WO-00351273",
+    "cliente": "Ayda Leany Samboni Beltran",
+    "estado": "Entregado",
+    "fecha": "4/04/2025"
+  },
+  {
+    "codigo": "WO-00351092",
+    "cliente": "Ximena Ochoa",
+    "estado": "Entregado",
+    "fecha": "3/04/2025"
+  },
+  {
+    "codigo": "WO-00351050",
+    "cliente": "Eliana Yineth Chacon Tejada",
+    "estado": "Entregado",
+    "fecha": "3/04/2025"
+  },
+  {
+    "codigo": "WO-00350999",
+    "cliente": "Andrea Tovar",
+    "estado": "Entregado",
+    "fecha": "3/04/2025"
+  },
+  {
+    "codigo": "WO-00350992",
+    "cliente": "Gabriel Antonio Goyes Urbano",
+    "estado": "Entregado",
+    "fecha": "3/04/2025"
+  },
+  {
+    "codigo": "WO-00348211",
+    "cliente": "Jorge Armando Trujillo",
+    "estado": "Entregado",
+    "fecha": "26/03/2025"
+  },
+  {
+    "codigo": "WO-00347174",
+    "cliente": "Willian Alberto Padilla Ariza",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347170",
+    "cliente": "Nury Perez",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347169",
+    "cliente": "Margoht Rodríguez",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347060",
+    "cliente": "Aider Díaz",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347057",
+    "cliente": "Silvia Fernanda Mosquera Rojas",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347043",
+    "cliente": "Alberto Betsancut",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347042",
+    "cliente": "Betania Byo Sas",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347035",
+    "cliente": "Karen Lizeth Vásquez Munar",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347034",
+    "cliente": "Ana Beiba Astudillo",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347030",
+    "cliente": "Juan Diego Laverde Ospina",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00347027",
+    "cliente": "Yineth Medina Vargas",
+    "estado": "Entregado",
+    "fecha": "21/03/2025"
+  },
+  {
+    "codigo": "WO-00344313",
+    "cliente": "Mesthil Ruiz Duran",
+    "estado": "Entregado",
+    "fecha": "11/03/2025"
+  },
+  {
+    "codigo": "WO-00344310",
+    "cliente": "Diana Marcela Córdoba Lopez",
+    "estado": "Entregado",
+    "fecha": "11/03/2025"
+  },
+  {
+    "codigo": "WO-00341622",
+    "cliente": "Kerly Alexandra Gutiérrez",
+    "estado": "Entregado",
+    "fecha": "28/02/2025"
+  },
+  {
+    "codigo": "WO-00341614",
+    "cliente": "Diego Fernando Manrrique Fernández",
+    "estado": "Entregado",
+    "fecha": "28/02/2025"
+  },
+  {
+    "codigo": "WO-00341610",
+    "cliente": "Carlos Alberto Martin",
+    "estado": "Entregado",
+    "fecha": "28/02/2025"
+  },
+  {
+    "codigo": "WO-00341608",
+    "cliente": "Karen Muñoz Urbano",
+    "estado": "Entregado",
+    "fecha": "28/02/2025"
+  },
+  {
+    "codigo": "WO-00336658",
+    "cliente": "Almacenes Exito S.a.",
+    "estado": "Entregado",
+    "fecha": "12/02/2025"
+  },
+  {
+    "codigo": "WO-00336649",
+    "cliente": "Almacenes Exito S.a.",
+    "estado": "Entregado",
+    "fecha": "12/02/2025"
+  }
+]
