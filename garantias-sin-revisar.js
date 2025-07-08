4e4078686c4ee935e20d9525d9f66348cebd50eb
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-07-07 22:17:32 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,6 +1,14 @@
 let garantiasSinRevisar = [
-'2025-06-25 - 10:37:17 - IRA561 - B&D - freidora black deckerr - FALLA: no prende ni calienta la señora ya la llevo a otro punto de servicio',
-'2025-06-28 - 15:48:16 - TBO541 - Garantía Universal - plancha corona - FALLA: se recalienta',
+    '2025-07-07 - 15:48:42 - VMT605 - B&D - licuadora blck decker ice crush - FALLA: no prende',
+    '2025-07-07 - 11:44:45 - RMG073 - Oster - licuadora oster 6 velocidades negra - FALLA: no enciende',
+    '2025-07-07 - 11:24:06 - KQK361 - Oster - arrocera oster negra - FALLA: la palanca o se sostiene en la parte de abajo',
+    '2025-07-05 - 09:40:42 - EFG854 - Universal - licuadora elite turbo max - FALLA: no sirve la tarjeta y el motor recalentado',
+    '2025-07-04 - 17:16:51 - IGH613 - B&D - plancha black decker suela titanio morada - FALLA: prende pero no calienta',
+    '2025-07-04 - 11:36:05 - GTP146 - B&D - licuadora black decker - FALLA: no funciona el teclado',
+    '2025-07-03 - 10:22:37 - MJU521 - Universal - licuadora universal mas vital - FALLA: quemada',
+    '2025-07-03 - 08:57:58 - SBL836 - Universal - freidora universal digital - FALLA: deja de calentar y no suena. nada',
+    '2025-07-02 - 14:39:17 - EVU186 - Universal - licuadora universal - FALLA: salio chispas',
+    '2025-06-25 - 10:37:17 - IRA561 - B&D - freidora black deckerr - FALLA: no prende ni calienta la señora ya la llevo a otro punto de servicio',
 ]
 
 // garantiasSinRevisar.reverse()
