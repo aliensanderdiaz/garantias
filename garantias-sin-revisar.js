b131e1494c62da864f79d01ab11e6a96c38df3ed
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-08-05 22:31:29 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,11 +1,11 @@
 let garantiasSinRevisar = [
-'2025-07-14 - 12:07:26 - SQP862 - Universal - plancha universal elite pro - FALLA: no calienta',
-'2025-07-14 - 09:34:57 - LKB004 - Universal - olla arrocera negra de 2 libras - FALLA: no prende es la tercera vez que la trae por garantía',
-'2025-07-12 - 10:38:02 - REA535 - Universal - licuadora mas vital - FALLA: no prende',
-'2025-07-12 - 10:30:16 - JUZ173 - B&D - olla arrocera de 2 libras - FALLA: no calienta',
-'2025-07-09 - 10:34:55 - WTY561 - Oster - licuadora oster clasica - FALLA: esta quemada',
-'2025-07-09 - 10:30:52 - VLZ938 - Oster - licuadora oster clasica - FALLA: no prende',
-'2025-07-04 - 17:16:51 - IGH613 - B&D - plancha black decker suela titanio morada - FALLA: prende pero no calienta',
+'2025-08-05 - 15:08:36 - YVR741 - B&D - plancha black decker titanio - FALLA: prende pero no calienta',
+'2025-08-05 - 14:34:00 - JRM428 - Oster - freidora oster de 6 litros digital - FALLA: no calienta',
+'2025-08-02 - 16:20:25 - PVN454 - Oster - plancha oster ceramica soleplate - FALLA: no prende',
+'2025-08-01 - 16:32:47 - NVG825 - B&D - batidora black Decker negra - FALLA: no prende',
+'2025-08-01 - 11:03:31 - VPI954 - Universal - licuadora universal potency mix roja - FALLA: sale mucha chispa',
+'2025-08-01 - 08:49:51 - PHS384 - Oster - exprimidor oster negro - FALLA: no tiene nada de fuerza',
+'2025-08-01 - 08:38:23 - QXX223 - Oster - licuadora oster reversible - FALLA: tarjeta no da continuidad',
 ]
 
 // garantiasSinRevisar.reverse()
