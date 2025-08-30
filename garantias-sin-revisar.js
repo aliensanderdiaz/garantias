034369b285fec7c5cbf7ae076ecda5fb0d0c3104
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-08-30 13:07:14 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,15 +1,14 @@
 let garantiasSinRevisar = [
-'2025-08-14 - 16:15:52 - BOU738 - Oster - olla arrocera oster - FALLA: no calienta',
-'2025-08-12 - 08:22:43 - MNP809 - Oster - extrator oster grande - FALLA: no prende',
-'2025-08-11 - 15:30:20 - LRO492 - Universal - ventilador universal dynamic - FALLA: no prende',
-'2025-08-06 - 17:56:26 - WXK718 - B&D - exprimidor blanco - FALLA: no tiene nada de potencia',
-'2025-08-06 - 10:31:46 - OSH326 - Universal - ventilador universal 3 en 1 dinami - FALLA: no prende',
-'2025-08-01 - 16:32:47 - NVG825 - B&D - batidora black Decker negra - FALLA: no prende',
-'2025-08-01 - 08:49:51 - PHS384 - Oster - exprimidor oster negro - FALLA: no tiene nada de fuerza',
+    '2025-08-30 - 11:55:40 - DPC220 - B&D - freidora de 4.5 litros - FALLA: la perilla del tiempo no corre y no pita cuando esta la comida',
+    '2025-08-29 - 09:07:37 - ZXJ936 - Oster - licuadora clasica - FALLA: las velocidades 1 y la 2 son muy lentas no tiene potencia se recibe solo motor',
+    '2025-08-29 - 08:57:32 - LNS640 - Oster - licuadora clasica - FALLA: no enciende el primera velocidad',
+    '2025-08-28 - 15:09:17 - MSA674 - Oster - licuadora oster reversible color negro - FALLA: sale chispa en el motor',
+    '2025-08-27 - 16:07:45 - BLR925 - Oster - licuadora oster - FALLA: no enciende',
+    '2025-08-27 - 15:47:54 - NWI858 - Oster - freidora 4 L. - FALLA: no enciende',
+    '2025-08-27 - 14:38:08 - UST119 - B&D - plancha - FALLA: al usar lo del vapor se le sale toda el agua',
+    '2025-08-26 - 16:04:29 - AAV987 - B&D - explimidor de naranjas marca black+decher - FALLA: motor recalentado',
 ]
-
 // garantiasSinRevisar.reverse()
-
+// 
 let garantiasExitoPitalito = [
-
 ]
