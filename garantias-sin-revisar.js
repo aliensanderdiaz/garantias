5bab1d15204b0909f8eebd4eea7a92beb1467f51
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-09-08 22:58:49 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,12 +1,9 @@
 let garantiasSinRevisar = [
-    '2025-08-30 - 11:55:40 - DPC220 - B&D - freidora de 4.5 litros - FALLA: la perilla del tiempo no corre y no pita cuando esta la comida',
-    '2025-08-29 - 09:07:37 - ZXJ936 - Oster - licuadora clasica - FALLA: las velocidades 1 y la 2 son muy lentas no tiene potencia se recibe solo motor',
-    '2025-08-29 - 08:57:32 - LNS640 - Oster - licuadora clasica - FALLA: no enciende el primera velocidad',
-    '2025-08-28 - 15:09:17 - MSA674 - Oster - licuadora oster reversible color negro - FALLA: sale chispa en el motor',
-    '2025-08-27 - 16:07:45 - BLR925 - Oster - licuadora oster - FALLA: no enciende',
-    '2025-08-27 - 15:47:54 - NWI858 - Oster - freidora 4 L. - FALLA: no enciende',
-    '2025-08-27 - 14:38:08 - UST119 - B&D - plancha - FALLA: al usar lo del vapor se le sale toda el agua',
-    '2025-08-26 - 16:04:29 - AAV987 - B&D - explimidor de naranjas marca black+decher - FALLA: motor recalentado',
+'2025-08-30 - 16:51:32 - THI911 - Universal - licuadora - FALLA: no prende y huele a quemado el motor - REPUESTOS: FALTA - OI: FALTA - FACTURA: FALTA',
+
+'2025-09-08 - 15:54:19 - THQ639 - Oster - sanduchera oster 3 en 1 - FALLA: no prende',
+
+'2025-09-08 - 12:00:39 - NHQ126 - Universal - freidora de aire - FALLA: no prende',
 ]
 // garantiasSinRevisar.reverse()
 // 
