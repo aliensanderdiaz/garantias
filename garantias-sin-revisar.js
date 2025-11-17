56a7b82dd917053d0d7697dd5cad46d17f4726ed
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-11-16 21:48:30 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,28 +1,6 @@
 let garantiasSinRevisar = [
-'2025-10-12 - 10:32:20 - VGI498 - Otros - sanduchera basic imusa negra - FALLA: el señor la trae partida la parte de la resistencia y dice que al apretar de le partio - Lugar de Compra: EXITO NEIVA',
-'2025-10-10 - 17:02:12 - WYD047 - Oster - freidora oster 3.8litros manual - FALLA: no funciona solo enciende el indicador - Lugar de Compra: LAGOBO',
-'2025-10-10 - 15:50:27 - IAC543 - Otros - ventilador samurai comfor negro - FALLA: no prende - Lugar de Compra: HOMECENTER',
-'2025-10-10 - 10:53:24 - JEK595 - Otros - ventilador samurai comfor - FALLA: motor recalentado - Lugar de Compra: ALMACENES UNIVERSAL',
-'2025-10-10 - 10:42:43 - XJP098 - Otros - ventilador samura air protec max negro - FALLA: motor en corto no da continuidad - Lugar de Compra: CENCOSUD COLOMBIA',
-'2025-10-10 - 10:15:03 - HNU861 - Otros - licuadora ultraforce - FALLA: la tarjeta no da continuidad - Lugar de Compra: NOVAVENTA',
-'2025-09-27 - 09:09:24 - COJ239 - Universal - plancha green colors - FALLA: el cliente manifiesta que le mancha la ropa con el agua que le echa y no quiere esa misma referencia (cambio de producto) - Lugar de Compra: ALMACEN UNIVERSAL',
-'2025-09-26 - 12:14:49 - OZS645 - Universal - termo bomba de 4 litros universal - FALLA: se recalienta por fuera y por dentro se enfria - Lugar de Compra: ALMACENES UNIO',
-'2025-09-23 - 11:57:32 - UJC882 - Oster - exprimidor de citricos oster - FALLA: no prende - Lugar de Compra: ALMACÉNES UNIVERSAL',
-'2025-09-19 - 09:43:36 - PWW623 - Oster - licuadora classic - FALLA: huele a quemado el motor - Lugar de Compra: TECNOHOGAR',
-'2025-09-18 - 14:46:55 - BKE343 - B&D - batidora - FALLA: no arranca en las velocidades y huele a quemado se recibe solo el motor con rayones y manchas de uso - Lugar de Compra: TECNOHOGAR',
-'2025-09-15 - 10:33:34 - XYJ954 - Otros - licuadora corona - FALLA: motor quemado - Lugar de Compra: D-TODO',
-'2025-09-15 - 10:09:49 - IQQ944 - Oster - licuadora - FALLA: motor huele a quemado - Lugar de Compra: MISCELANEA JIREH',
-'2025-09-15 - 08:57:23 - QHJ091 - Universal - freidora 3.4 - FALLA: no calienta nada - Lugar de Compra: ÚNICO',
-'2025-09-13 - 10:02:34 - JOD364 - Oster - cafetera - FALLA: no funciona al encender no da calor - Lugar de Compra: EXITO',
-'2025-09-11 - 11:59:56 - VYG739 - Oster - licuadora oster - FALLA: le salio humo por el motor y no volvio a prender - Lugar de Compra: TECNOHOGAR DE COLOMBIA',
-'2025-09-11 - 11:09:30 - LYA281 - Oster - freidora 7.5 digital - FALLA: enciende pero no calienta ni funciona el ventilador - Lugar de Compra: ÉXITO PITALITO',
-'2025-09-10 - 17:19:44 - IRP073 - Oster - arrocera oster - FALLA: no prende no calienta - Lugar de Compra: METRO PITALITO',
-'2025-09-09 - 10:57:50 - SPT504 - Oster - freidora 3.8L - FALLA: el cliente manifiesta que no calienta lo suficiente pará llevar a termino los alimentos - Lugar de Compra: EXITO PITALITO',
-'2025-09-09 - 10:33:23 - TPT347 - B&D - cafetera 5 tazas - FALLA: no bota el agua - Lugar de Compra: TENOHOGAR DE COLOMBIA',
-'2025-09-08 - 15:54:19 - THQ639 - Oster - sanduchera oster 3 en 1 - FALLA: no prende - Lugar de Compra: ALMACEN UNIVERSAL',
-'2025-09-08 - 12:00:39 - NHQ126 - Universal - freidora de aire - FALLA: no prende - Lugar de Compra: TECNOHOGAR DE COLOMBIA',
-
-
+'2025-11-15 - 12:52:37 - RRS986 - Universal - ventialdo universal dynamic de mesa - FALLA: no prendio mas',
+'2025-11-12 - 16:23:07 - KDH360 - Oster - batidora de inmersion oster - FALLA: el cliente mensiona que no tiene fuerza',
 ]
 // garantiasSinRevisar.reverse()
 // 
