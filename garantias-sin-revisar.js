badbfac0944c59ee4db1e462dc6654911d9381e4
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-12-09 20:26:28 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -1,11 +1,12 @@
 let garantiasSinRevisar = [
+'2025-12-06 - 12:01:51 - PGY112 - Oster - motor licuadora oster - FALLA: el cliente menciona que hay que girar el cuadrante para que prenda y cuando le pone el vaso con su fruta para hacer el jugo no arranca como debe ser es muy lento',
+'2025-12-06 - 10:05:45 - JTW648 - Oster - freidora oster 3.8 litros - FALLA: el cliente menciona que se empieza a deformar por el uso y calor del mismo, no calienta en el area de la bandeja',
+'2025-12-05 - 15:29:36 - JTS809 - Universal - estufa electrica 2 puestos - FALLA: el quemador izquierdo no funciona',
+'2025-12-04 - 16:12:31 - DLN007 - Oster - freidora oster - FALLA: no prende por ratos',
+'2025-12-04 - 13:38:04 - QPX449 - Oster - fridora oster 4 litros manual - FALLA: la freidora enciende pero no realiza su funcion las comidas quedan crudas',
+'2025-12-04 - 09:34:02 - VXS633 - Oster - exprimidor oster citrico - FALLA: no prende',
 '2025-11-26 - 11:31:09 - CBU388 - Oster - freidora oster de 9 litrod digital - FALLA: no prende',
 
-'2025-12-04 - 16:12:31 - DLN007 - Oster - freidora oster - FALLA: no prende por ratos',
-
-'2025-12-04 - 13:38:04 - QPX449 - Oster - fridora oster 4 litros manual - FALLA: la freidora enciende pero no realiza su funcion las comidas quedan crudas',
-
-'2025-12-04 - 09:34:02 - VXS633 - Oster - exprimidor oster citrico - FALLA: no prende',
 ]
 // garantiasSinRevisar.reverse()
 // 
