91067ca6c014dfc30a8e9c745e899f13623300c7
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-09-02 21:30:34 -0500

    m

--- garantias-sin-revisar.js
+++ garantias-sin-revisar.js
@@ -4,172 +4,18 @@
 
 
 let garantiasSinRevisar = [
-{
-"_id": "6a80cc682924e7f14e5f8d8e",
-"fechaDeIngreso": "2026-08-15",
-"horaDeIngreso": "15:30:31",
-"codigo": "CKK840",
-"producto": "moledor automatico universal de sal y pimienta",
-"falla": "no prende",
-"referencia": "L63270",
-"lugarDeCompra": "NOVAVENTA",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a84778da431d7f0df89650d",
-"fechaDeIngreso": "2026-08-18",
-"horaDeIngreso": "10:17:32",
-"codigo": "HFS248",
-"producto": "olla a presion ultra max",
-"falla": "se le sale todo el agua por la válvula de segurida y el cliente dice que ya la a cambiando",
-"referencia": "L25675",
-"lugarDeCompra": "UNICO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a88d165dbbebb2644fcb4bb",
-"fechaDeIngreso": "2026-08-21",
-"horaDeIngreso": "17:29:54",
-"codigo": "MRG029",
-"producto": "ventilador universal",
-"falla": "prende en la tercera",
-"referencia": "L75680",
-"lugarDeCompra": "COMERCIAL BAHIA",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8a15c8d1b102c165ec80bb",
-"fechaDeIngreso": "2026-08-22",
-"horaDeIngreso": "16:33:59",
-"codigo": "HWS901",
-"producto": "licuadora  universal tradicional",
-"falla": "motor chispas",
-"referencia": "L50803",
-"lugarDeCompra": "LA FERIA DEL ALUMINIO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8c51bf27d1907f3b47eb87",
-"fechaDeIngreso": "2026-08-24",
-"horaDeIngreso": "09:14:21",
-"codigo": "AML965",
-"producto": "freidora universal digital",
-"falla": "no prende y se demoraba para asar",
-"referencia": "L87580",
-"lugarDeCompra": "UNICO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8c562d27d1907f3b47eb96",
-"fechaDeIngreso": "2026-08-24",
-"horaDeIngreso": "09:33:15",
-"codigo": "ZYY232",
-"producto": "plancha universal seca",
-"falla": "se le safo la perilla",
-"referencia": "L40600",
-"lugarDeCompra": "ALKOMPRAR",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8cbad5996d09c0e12d2a1e",
-"fechaDeIngreso": "2026-08-24",
-"horaDeIngreso": "16:42:34",
-"codigo": "ZRX198",
-"producto": "licuadora de 8",
-"falla": "no prende",
-"referencia": "BLSTPEG-GPB-013",
-"lugarDeCompra": "METRO PITALITO",
-"tipoDeServicio": "Garantía Oster"
-},
-{
-"_id": "6a8cc5ab996d09c0e12d2a39",
-"fechaDeIngreso": "2026-08-24",
-"horaDeIngreso": "17:28:56",
-"codigo": "AYU091",
-"producto": "ventilador universal digital",
-"falla": "no prende",
-"referencia": "L76680",
-"lugarDeCompra": "UNICO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8efa8bc0e5497b3f31cbf2",
-"fechaDeIngreso": "2026-08-26",
-"horaDeIngreso": "09:39:06",
-"codigo": "CGS975",
-"producto": "freidora oster de 3,8 litros",
-"falla": "prende pero no calienta la señora dice que es la segunda vez",
-"referencia": "CKSTAF38MNS-014",
-"lugarDeCompra": "HOMECENTER",
-"tipoDeServicio": "Garantía Oster"
-},
-{
-"_id": "6a8efd67c0e5497b3f31cc01",
-"fechaDeIngreso": "2026-08-26",
-"horaDeIngreso": "09:51:18",
-"codigo": "GAJ714",
-"producto": "ventilador universal digital",
-"falla": "motor quemado",
-"referencia": "L76680",
-"lugarDeCompra": "LANDER UNICO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a8f6626f2277a1e1a16d254",
-"fechaDeIngreso": "2026-08-26",
-"horaDeIngreso": "17:18:13",
-"codigo": "AXL459",
-"producto": "freidora universal",
-"falla": "no prende",
-"referencia": "L87560",
-"lugarDeCompra": "UNICO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a919a515170049a38ecd81d",
-"fechaDeIngreso": "2026-08-28",
-"horaDeIngreso": "09:25:19",
-"codigo": "VJN431",
-"producto": "licuadora universal de teclas negra",
-"falla": "sale chispas en el motor",
-"referencia": "L50803",
-"lugarDeCompra": "OLÍMPICA",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a9331b49a899bfeb60c0273",
-"fechaDeIngreso": "2026-08-29",
-"horaDeIngreso": "14:23:30",
-"codigo": "KBQ219",
-"producto": "lucuadora universal potency",
-"falla": "sale chispas",
-"referencia": "L50861",
-"lugarDeCompra": "CRISTALERIA EL HOGAR MODERNO",
-"tipoDeServicio": "Garantía Universal"
-},
-{
-"_id": "6a95b0e28f75d752a52f2894",
-"fechaDeIngreso": "2026-08-31",
-"horaDeIngreso": "11:50:41",
-"codigo": "EXM030",
-"producto": "freidora de aire 3,8 oster",
-"falla": "no enciende",
-"referencia": "CKSTAF38MNS-014",
-"lugarDeCompra": "ARA",
-"tipoDeServicio": "Garantía Oster"
-},
-{
-"_id": "6a95d5b7fc52b07fa9ff5c76",
-"fechaDeIngreso": "2026-08-31",
-"horaDeIngreso": "14:27:49",
-"codigo": "OWK414",
-"producto": "plancha vertocal pro",
-"falla": "no calienta bien no sale vapor",
-"referencia": "L95830",
-"lugarDeCompra": "UNICO LANDER",
-"tipoDeServicio": "Garantía Universal"
-}
-]
+    {
+      "_id": "6a98a347ec1b3e14d69e21d4",
+      "fechaDeIngreso": "2026-09-02",
+      "horaDeIngreso": "17:29:25",
+      "codigo": "GYO855",
+      "producto": "ventilador universal digital",
+      "falla": "se frena",
+      "referencia": "L76680",
+      "lugarDeCompra": "UNICO",
+      "tipoDeServicio": "Garantía Universal"
+    }
+  ]
 
 
 
@@ -184,9 +30,6 @@ let garantiasExitoPitalito = [
 
     // '2026-03-28 - 16:35:22 - WO-00447984 - HTL592 - Garantía Oster - freidora oster de 7.5 digital - FALLA: no calienta - Lugar de Compra: HOMECENTER'
 
-'2026-08-08 - 10:45:58 - GUT507 - GRT-78749 - Garantía Universal - ventilador universal digital - FALLA: se frena - MOTOR',
-'2026-08-10 - 11:35:36 - ZFA049 - GRT-78752 - Garantía Universal - ventilado universal pequeño - FALLA: se quemo - MOTOR',
-'2026-08-12 - 15:16:46 - HYA392 - GRT-78748 - Garantía Universal - licuadora la tradicional - FALLA: motor se partio y el acople de la cuchilla lo daño - EMBRAGUE Y CUCHILLA'
 
 ]
 
